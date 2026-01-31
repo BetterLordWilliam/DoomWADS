0996bd3f2d5ccd1fd015a2d4efc157bddd8836fe
LordWilliam <willjordan05@icloud.com>
2026-01-30 23:47:52 -0800

    WO: added all the WAD files to the gitignore as these should be seperate resources, but the tooling however is common to them all and so shall be located in this parent directory

---
+++ env.ps1
@@ -0,0 +1,4 @@
+$env:DOOM2 = "C:\Program Files\UZDoom\game_wads\DOOM2.WAD"
+$env:HEXEN = "C:\Program Files\UZDoom\game_wads\hexen.wad"
+$env:HERETIC = "C:\Program Files\UZDoom\game_wads\heretic.wad"
+
---
+++ start.ps1
@@ -0,0 +1,22 @@
+param(
+  [switch]$doom,
+  [switch]$hex,
+  [switch]$her,
+  [switch]$tmp
+)
+
+. .\env.ps1
+
+if ( $doom ) {
+  if ( $tmp ) {
+    uzdoom -file .\ ..\Maps -iwad $env:DOOM2 -stdout +map mp_map01
+  } else {
+    uzdoom -file .\ ..\Maps -iwad $env:DOOM2 -stdout
+  }
+} elseif ( $hex ) {
+  uzdoom -file .\ ..\Maps -iwad $env:HEXEN -stdout
+} elseif ( $her ) {
+  uzdoom -file .\ ..\Maps -iwad $env:HERETIC -stdout
+}
+
+
